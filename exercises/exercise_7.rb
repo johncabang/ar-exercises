d6837dd4f0248c1e97b8f8aa3c8e645765090b49
johncabang <johncabz@gmail.com>
2020-11-10 20:26:52 +0000

    Exercise_7 done validations for both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,10 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Enter a store name: "
+@store_name = gets.chomp
+
+Store.create(name: @store_name)
+# The errors.full_messages method returns the error messages in a user-friendly format, with the capitalized attribute name prepended to each message
+puts @store_name.errors.full_messages
